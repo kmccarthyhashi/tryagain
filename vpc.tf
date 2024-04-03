bc0c8e30cdb14eca181b7b8fb14205be33b6af1f
kmccarthyhashi <107080002+kmccarthyhashi@users.noreply.github.com>
2024-04-03 13:56:50 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,7 @@
 # VPC creation
 resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
+  # cidr_block           = "10.0.0.0/16" #private
+  cidr_block           = "178.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
@@ -41,17 +42,49 @@ resource "aws_subnet" "public_subnets" {
   }
 }
 
+# ADDED
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+   }
+ }
+
+# Associate private subnets to route table
+resource "aws_route_table_association" "public_subnet_asso" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.public-route-table.id
+}
+
 # Security group allowing traffice from anywhere to port 80
 resource "aws_security_group" "load-balancer" {
   name        = "load_balancer_security_group"
-  description = "Controls access to the ELB"
+  description = "Controls access to the ELB: allow ssh http inbound traffic"
   vpc_id      = aws_vpc.main.id
 
   ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description = "HTTP from VPC"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
@@ -59,10 +92,25 @@ resource "aws_security_group" "load-balancer" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
+
+  # ingress {
+  #  from_port   = 80
+  #  to_port     = 80
+  #   protocol    = "tcp"
+  #  cidr_blocks = ["0.0.0.0/0"]
+  # }
+
+  # egress {
+  #  from_port   = 0
+  #  to_port     = 0
+  #  protocol    = "-1"
+  #  cidr_blocks = ["0.0.0.0/0"]
+  # }
 }
 
-# An internet facing ELB with connections to the 3 private subnet IP addresses of your web servers
+# An internet facing ELB with connections to the 3 public subnet IP addresses of your web servers
 resource "aws_elb" "elb" {
   name            = "section1-elb"
   security_groups = [aws_security_group.load-balancer.id]
