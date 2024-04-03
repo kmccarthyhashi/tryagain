dee2fcf884652cc33022b3b35c44e967addb46ff
kmccarthyhashi <107080002+kmccarthyhashi@users.noreply.github.com>
2024-04-03 14:20:05 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,31 @@
 # launch template used to create instances and autoscaling group
-resource "aws_launch_template" "amitemp" {
-  name_prefix   = "section1"
-  vpc_security_group_ids = [aws_security_group.load-balancer.id]
-  image_id      = "ami-0a55cdf919d10eac9"
-  instance_type = "t2.micro"
-  placement {
-    availability_zone = "us-west-2a"
-  }
-}
+#resource "aws_launch_template" "amitemp" {
+#  name_prefix   = "section1"
+#  vpc_security_group_ids = [aws_security_group.load-balancer.id]
+#  image_id      = "ami-0a55cdf919d10eac9"
+#  instance_type = "t2.micro"
+#  placement {
+#    availability_zone = "us-west-2a"
+#  }
+#}
 
-resource "aws_autoscaling_group" "ec2-cluster" {
-  desired_capacity   = 3
-  max_size           = 5
-  min_size           = 3
-  health_check_type  = "EC2"
+#resource "aws_autoscaling_group" "ec2-cluster" {
+#  desired_capacity   = 3
+#  max_size           = 5
+#  min_size           = 3
+#  health_check_type  = "EC2"
   # vpc_zone_identifier = [aws_subnet.private_subnets[0].id, aws_subnet.private_subnets[1].id, aws_subnet.private_subnets[2].id]
-  vpc_zone_identifier = [aws_subnet.public_subnets[0].id, aws_subnet.public_subnets[1].id, aws_subnet.public_subnets[2].id]
-  target_group_arns = [aws_alb_target_group.default-target-group.arn]
-  # took out azs and replaced with vpc_zone_identifier
+#  vpc_zone_identifier = [aws_subnet.public_subnets[0].id, aws_subnet.public_subnets[1].id, aws_subnet.public_subnets[2].id]
+#  target_group_arns = [aws_alb_target_group.default-target-group.arn]
+#  # took out azs and replaced with vpc_zone_identifier
   # connects vpc in autoscaling group
 
   # launch_template now preferred to launch configuration
-  launch_template {
-    id      = aws_launch_template.amitemp.id
-    version = "$Latest"
+#  launch_template {
+#    id      = aws_launch_template.amitemp.id
+#    version = "$Latest"
 
-  }
+#  }
 }
 
 # EC2 instances
@@ -79,8 +79,8 @@ resource "aws_security_group" "ec2" {
 }
 
 # attach ec2 instances to autoscaling group
-resource "aws_autoscaling_attachment" "asg_attachment_bar" {
-  autoscaling_group_name = aws_autoscaling_group.ec2-cluster.id
-  lb_target_group_arn    = aws_alb_target_group.default-target-group.arn
-}
+#resource "aws_autoscaling_attachment" "asg_attachment_bar" {
+#  autoscaling_group_name = aws_autoscaling_group.ec2-cluster.id
+#  lb_target_group_arn    = aws_alb_target_group.default-target-group.arn
+#}
 
